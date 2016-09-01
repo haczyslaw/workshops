8916198019022af0d0b7a0129ad519676eab8ea0
ahaczyk <haczyslaw@gmail.com>
2016-09-02 00:20:49 +0200

    Add subject member route to teachers resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "students#index"
-  resources :teachers
+
+  resources :teachers do
+    get :subjects
+  end
+
   get "/reports/subjects", to: "reports#subjects", as: "report_subjects"
 
   resources :students do
