84ffc7b8434e5d0872d6d05b409bf3234a188c79
ahaczyk <haczyslaw@gmail.com>
2016-09-01 20:36:27 +0200

    Add root_path, teachers_path and report_subjects_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root "students#index"
+  resources :teachers
+  get "/reports/subjects", to: "reports#subjects", as: "report_subjects"
+
   resources :students do
     get :subjects
   end
